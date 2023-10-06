3f2e5c36f08ffdfc6f4ffa5c5f8f569b76e9ab4d
abrahamvg <abrageor06@gmail.com>
2023-10-07 01:06:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import Home from './components/Home';
-=======
 import Navbar from './components/Navbar';
 import FirstDiv from './components/landingpage/firstDiv';
 import SecondDiv from './components/landingpage/secondDiv';
@@ -10,14 +7,10 @@ import FourthDiv from './components/landingpage/FourthDiv';
 import FifthDiv from './components/landingpage/FifthDiv';
 import SixthDiv from './components/landingpage/SixthDiv';
 import Rabbit from './components/animals/rabbit';
->>>>>>> b60d4b7c53975e1e3138a1d81b38e91edfb57659
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Home/>
-=======
       <Navbar/>
       <FirstDiv/>
       <SecondDiv/>
@@ -26,7 +19,6 @@ function App() {
       <FifthDiv/>
       <SixthDiv/>
       <Rabbit/>
->>>>>>> b60d4b7c53975e1e3138a1d81b38e91edfb57659
     </div>
   );
 }
